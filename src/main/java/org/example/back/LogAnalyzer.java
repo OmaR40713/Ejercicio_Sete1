92a2b74d7ed02989de545ab4a39b81e85dc0edf6
alsay <alsayedsuleimanomar@gmail.com>
2025-10-21 20:09:15 +0200

    haciendo el LogAnalyzer

--- src/main/java/org/example/back/LogAnalyzer.java
+++ src/main/java/org/example/back/LogAnalyzer.java
@@ -1,4 +1,15 @@
 package org.example.back;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class LogAnalyzer {
+    public static void main(String[] args) {
+
+        Scanner sr = new Scanner(System.in);
+        String  operation=args[0];
+        Path path =Path.of(args[1]);
+
+    }
 }
